50e2ba776a08798989ef6dbed782dc553127dc76
MD ARIF KHAN <arifthehappy@gmail.com>
2024-03-27 14:58:50 +0530

    ta3

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,28 @@ import { roomHandler } from "./room";
 
 const port = 8000 || process.env.PORT;
 const app = express();
+
+// Set middleware of CORS 
+app.use((_req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 app.use(cors);
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
